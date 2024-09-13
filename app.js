a8cb72d404f259f198fe9f622cca20db0e0557ff
Owais Malik <hafiz.owais1121681@gmail.com>
2024-09-13 22:37:55 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+let mile = Number(prompt("Enter Value Of Mile"));
+let km = 1.60934;
+let result = km * mile;
+
+document.getElementById("displayMile").innerHTML = mile;
+document.getElementById("displayKm").innerHTML = result;
